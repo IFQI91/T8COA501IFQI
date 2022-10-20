2a14b897a772922e9ce8d8836ba94d44451acae0
IFQI91 <ifqi90791@gmail.com>
2022-10-19 21:02:02 -0500

    tarea8

---
+++ T8COA501IFQI.R
@@ -0,0 +1,27 @@
+
+##Tarea 8
+##COA-501 Herramientas de cómputo para investigadores (R y Python)
+## Alumno: Ivan Fermin Quiroz Ibañez
+
+
+#1. Realice un análisis de componentes principales ACP con el conjunto de datos Boston.csv adjunto
+#y el script PCA proporcionado. Escale el conjunto de características, calcule y grafique la matriz
+#de covarianzas, la contribución de cada componente principal (PC) para explicar la varianza de
+#los datos, grafique PC1 vs PC2, y PC1 vs PC3. Comente si es conveniente efectuar una reducción
+#de dimensionalidad del conjunto de datos Boston.
+
+
+#2. Realice una regresión con componentes principales con el conjunto de datos Boston.csv. Con
+#base en el script proporcionado. Utilice como variable dependiente la mediana del precio de
+#vivienda. Elabore una gráfica con los residuales del modelo (eje Y) versus los valores predichos
+#(eje X). y una gráfica valores predichos versus valores observados. Reporte el resumen estadístico
+#del modelo. Comente sus resultados.
+
+
+#3. Busque conjuntos de datos (en internet o cualquier libro de diseño de experimentos) para aplicar
+#la prueba de Wilcoxon y modifique el script visto en clase
+#a) Comparar dos muestras de datos apareados
+#b) Comparar dos muestras de datos independientes
+#c) Comparar los tratamientos de un diseño completamente al azar.
+#d) Reporte igualmente las gráficas de boxplot a colores de los incisos a, b y c.
+
